dbf8d11be9907a3cc88068c7b7283c9da6fd8868
MuhAndriansyah <muh.andriansyah97@gmail.com>
2020-09-07 16:47:19 +0800

    menambahkan redux beserta keluarganya

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 /* eslint-disable space-before-function-paren */
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import 'upkit/dist/style.min.css'
+import {} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Learn React
-        </button>
-      </header>
-    </div>
-  )
+  return <div className="App">
+    <Home />
+  </div>
 }
 
 export default App
